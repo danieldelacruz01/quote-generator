964aebfcb4f36e2033d09c3c22e84a39cfa026c8
Daniel de la Cruz <danieldelacruz01@gmail.com>
2016-02-04 22:31:57 +1300

    add css styling

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 $(document).ready(function(){
+
 var number; 
 var data = {
   "quotes": [{
     "quote": "They keep saying 'there are plenty more fish in the sea.' I say fuck you she was my sea.",
     "name": "Christopher Poindexter",
-    "img": "img/p2.jpg"
+    "img": "images/winstonchurchill.jpg"
       },
   {
     "quote": "Success consists of going from failure to failure without loss of enthusiasm.",
     "name": "Winston Churchill",
-    "img": "img/p2.jpg"
+    "img": "images/winstonchurchill.jpg"
   },
   {
     "quote": "Two things are infinite: the universe, and human stupidity.... and I'm not sure about the universe.",
     "name": "Albert Einstein",
-    "img": "img/p2.jpg"
+    "img": "images/winstonchurchill.jpg"
   }]
-  
-}
+};
+
 $("button").click(function(){
     getQuote();
-                  });
+});
 
 function random(){
    number = Math.floor(Math.random()*data.quotes.length)
 
 }
  function display(){
-    $("#quote").html(data.quotes[number].quote);
-   $("#name").html("~ " + data.quotes[number].name);
-   $("#imagedrop").attr("src", data.quotes[number].img) 
+   $("#quote").html(data.quotes[number].quote);
+   $("#name").html(data.quotes[number].name);
+   $("#imagedrop").attr("src", data.quotes[number].img); 
  }
 
 function getQuote(){
@@ -37,6 +38,8 @@ function getQuote(){
   display();
   
 }
+
+getQuote();
 
 });
 
